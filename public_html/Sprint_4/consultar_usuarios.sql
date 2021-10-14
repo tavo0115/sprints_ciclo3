607380fd4bf0c5e416cd52ad724257eb1150a4d6
tavo0115 <tavo0115@gmail.com>
2021-10-13 22:31:30 -0500

    html sprint 5
    Versión 20211013

--- public_html/Sprint_4/consultar_usuarios.sql
+++ public_html/Sprint_4/consultar_usuarios.sql
@@ -1,10 +1,2 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  Gustavo Vargas
- * Created: 7/10/2021
- */
-
+SELECT *
+FROM usuarios;
--- public_html/Sprint_4/consultar_usuarios_desc.sql
+++ public_html/Sprint_4/consultar_usuarios_desc.sql
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  Gustavo Vargas
- * Created: 7/10/2021
- */
-
+SELECT id, usuario
+FROM usuarios
+ORDER BY id desc;
